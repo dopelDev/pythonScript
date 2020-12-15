267097959a690ddf55ab6250127d4f7f8851b56f
dopelDev <322kuroneko@gmail.com>
2020-12-14 22:40:12 -0500

    ordenando

---
+++ piglet/Piglet/Piglet.py
@@ -0,0 +1,27 @@
+from requests import get
+from requests.exceptions import (HTTPError, MissingSchema, ConnectionError)
+
+
+class piglet(object):
+    """docstring for piglet.
+        base de los scrapies"""
+
+    def __init__(self):
+        print('__init__.piglet')
+        self.http = ''
+        self.code = ''
+        self.connectionSucess = False
+
+    def addUrl(self, url):
+        try:
+            self.html = get(url)
+            self.code = self.html.status_code
+
+        except (ConnectionError, MissingSchema, HTTPError) as error:
+            print(error)
+
+    def confirmEnabled(self):
+        if self.code == 200:
+            self.connectionSucess = True
+        else:
+            self.connectionSucess = False
