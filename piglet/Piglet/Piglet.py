abc33238659a96d27d79975ce0d1a6430ae9c8db
dopelDev <322kuroneko@gmail.com>
2020-12-17 04:39:22 -0500

    piglet def getAurl

--- piglet/Piglet/Piglet.py
+++ piglet/Piglet/Piglet.py
@@ -1,5 +1,9 @@
 from requests import get
 from requests.exceptions import (HTTPError, MissingSchema, ConnectionError)
+from bs4 import BeautifulSoup
+"""
+commends of imports
+"""
 
 
 class piglet(object):
@@ -11,6 +15,7 @@ class piglet(object):
         self.http = ''
         self.code = ''
         self.connectionSucess = False
+        self.error = None
 
     def addUrl(self, url):
         try:
@@ -18,10 +23,49 @@ class piglet(object):
             self.code = self.html.status_code
 
         except (ConnectionError, MissingSchema, HTTPError) as error:
-            print(error)
+            self.error = error
 
     def confirmEnabled(self):
         if self.code == 200:
             self.connectionSucess = True
         else:
             self.connectionSucess = False
+
+    def returnHtml(self):
+        """Short summary.
+
+        Returns
+        -------
+        type
+            un str para ser sopeado.
+
+        """
+        if self.connectionSucess:
+            return self.html.text
+        else:
+            return self.error
+
+
+class sopero(object):
+    """docstring for sopero.
+        requiere de bs4
+        """
+
+    def __init__(self):
+        print('__init__.sopero')
+        self.listaPrima = []
+
+    def getAurl(self, html):
+        BigSopa = BeautifulSoup(html, 'html.parser')
+
+        for url in BigSopa.find_all('a'):
+            if url is None:
+                continue
+            elif url == '':
+                continue
+            elif len(url) < 2:
+                continue
+            else:
+                self.listaPrima.append(url.get('href'))
+
+        return self.listaPrima
