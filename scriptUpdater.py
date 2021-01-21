fd962468cf59a13a3b846f188fd9d545f24cbf9a
dopelDev <322kuroneko@gmail.com>
2021-01-21 12:49:20 -0500

    add1

--- scriptUpdater.py
+++ scriptUpdater.py
@@ -12,3 +12,27 @@
 #Facebook : https://www.facebook.com/profile.php?id=100036185774355
 #Mail : 322kuroneko2@gmail.com
 ################################################
+from sys import executable
+import os
+
+print(executable)
+
+def get_list_scripts(path):
+    script_list = []
+    if os.path.exists(path):
+        script_list = os.listdir(path)
+
+    return script_list
+
+# paths
+from_path_python = '/home/dopel/projects/pythonScript'
+from_path_bash = '/home/dopel/projects/bashScripts'
+destination_path = '/home/dopel/scripts'
+
+# lists
+list_python = get_list_scripts(from_path_python)
+list_bash = get_list_scripts(from_path_bash)
+list_destination  = get_list_scripts(destination_path)
+print(list_python)
+print(list_bash)
+print(list_destination)
